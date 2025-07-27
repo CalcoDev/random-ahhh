cc574247e0dd79db1c0073c35fd9fc56d51f7191
calcodev <calcopoddev@gmail.com>
2025-07-27 18:24:55 +0300

    set up git files + implemented something m neighbour tracing

---
+++ README.md
@@ -0,0 +1,3 @@
+# random ahhh
+
+A collection of literally fully random projects and experiments. Doing them all in a single project cuz it motivates me more lmao.
--- asriel/scripts/messin_around.gd
+++ asriel/scripts/messin_around.gd
@@ -64,33 +64,70 @@ func _generate_zones() -> void:
     #     line.add_point(pos + Vector2.RIGHT * 8.0)
 
     for marker in zones:
-        var outline_partitions := {}
-        var outline := _generate_zone_outline(marker.global_position, outline_partitions, cell_to_partition)
+        var prev_pixel := Vector2i.ZERO
+        var start_pos := map.local_to_map(map.to_local(marker.global_position))
+        while not _is_solid(start_pos + Vector2i.RIGHT):
+            start_pos += Vector2i.RIGHT
+            _draw_tile(start_pos, Color.HOT_PINK)
+        prev_pixel = start_pos
+        start_pos += Vector2i.RIGHT
 
-        # for cell in outline:
-        #     var line = _spawn_line(line_child, 16.0, colors[cell_to_partition[cell]])
-        #     var pos := map.to_global(map.map_to_local(cell))
-        #     line.add_point(pos - Vector2.RIGHT * 8.0)
-        #     line.add_point(pos + Vector2.RIGHT * 8.0)
+        var outline := _trace(start_pos, prev_pixel)
+        for cell in outline:
+            var partition: int = cell_to_partition[cell]
+            var line = _spawn_line(line_child, 16.0, colors[partition])
+            var pos := map.to_global(map.map_to_local(cell))
+            line.add_point(pos - Vector2.RIGHT * 8.0)
+            line.add_point(pos + Vector2.RIGHT * 8.0)
 
-        for partition in outline_partitions:
-            var start_pos: Vector2i = outline_partitions[partition]
-            var ordered_outline = _generate_ordered_outline(outline, start_pos, cell_to_partition)
 
-            for cell in ordered_outline:
-                var line = _spawn_line(line_child, 16.0, colors[partition])
-                var pos := map.to_global(map.map_to_local(cell))
-                line.add_point(pos - Vector2.RIGHT * 8.0)
-                line.add_point(pos + Vector2.RIGHT * 8.0)
+func _trace(start: Vector2i, enter: Vector2i) -> Array[Vector2i]:
+    var path: Array[Vector2i] = [start]
+    var pivot := start
+    var current := enter
+    var previous := start
+    var step := 0
+    while step == 0 or pivot != start:
+        step += 1
+        var n := _get_neighbourhood(current - pivot)
+        for off in n:
+            previous = current
+            current = pivot + off
+            if _is_solid(current):
+                path.append(current)
+                pivot = current
+                current = previous
+                break
+    return path
 
-            # for _i in 4:
-            #     var line := _spawn_line(line_child2, 1.0, Color.BLACK)
-            #     for i in ordered_outline.size():
-            #         var tile_pos: Vector2i = ordered_outline[i]
-            #         var global_pos := map.to_global(map.map_to_local(tile_pos))
-            #         var normal_dir := _get_outline_outer_normal(tile_pos)
-            #         line.add_point(global_pos + normal_dir * randf_range(1.0, 8.0))
-            #     line.add_point(line.points[0])
+
+# TODO(calco): this needs MASSIVE improvement lol
+func _get_neighbourhood(diff: Vector2i) -> Array[Vector2i]:
+    var a: Array[Vector2i] = [
+        Vector2i(-1, -1),
+        Vector2i(0, -1),
+        Vector2i(1, -1),
+        Vector2i(1, 0),
+        Vector2i(1, 1),
+        Vector2i(0, 1),
+        Vector2i(-1, 1),
+        Vector2i(-1, 0),
+    ]
+
+    var idx := a.find(diff)
+    assert(idx != -1, "what the fuck.")
+    var b: Array[Vector2i] = []
+
+    b.append_array(a.slice(idx + 1))
+    b.append_array(a.slice(0, idx))
+    return b
+
+
+func _get_edge_contour() -> void:
+    pass
+
+func _is_solid(pos: Vector2i) -> bool:
+    return map.get_cell_tile_data(pos) != null
 
 func _get_outline_outer_normal(tile_pos: Vector2i) -> Vector2i:
     var normal := Vector2i.ZERO
@@ -104,79 +141,11 @@ func _get_outline_outer_normal(tile_pos: Vector2i) -> Vector2i:
         normal += Vector2i.DOWN
     return normal
 
-func _generate_ordered_outline(outline: Dictionary, start_pos: Vector2i, cell_to_partition: Dictionary) -> Array:
-    var tiles := []
-
-    var og_start := start_pos
-
-    var yes := [start_pos, start_pos + Vector2i.DOWN, start_pos + Vector2i.LEFT + Vector2i.DOWN]
-    # if map.get_cell_tile_data(start_pos) and map.get_cell_tile_data(
-
-    var failed_count := 0
-    while failed_count < 4:
-        failed_count = 0
-        for d in [Vector2i.UP, Vector2i.LEFT, Vector2i.DOWN, Vector2i.RIGHT]:
-            og_start = start_pos
-            # print("DIR ", d, " START ", start_pos)
-            start_pos = _ordered_outline_4d_yeet(tiles, outline, start_pos, d)
-            failed_count += int(start_pos == og_start)
-            # print(" -=-=- ")
-
-    if tiles.size() == 0 and og_start == start_pos:
-        tiles.append(start_pos)
-        outline.erase(start_pos)
-        # print("Erasing aaa", start_pos)
-    
-    _ordered_outline_8d_yeet(tiles, outline, start_pos, Vector2i.LEFT + Vector2i.UP, cell_to_partition)
-    _ordered_outline_8d_yeet(tiles, outline, start_pos, Vector2i.LEFT + Vector2i.DOWN, cell_to_partition)
-    _ordered_outline_8d_yeet(tiles, outline, start_pos, Vector2i.RIGHT + Vector2i.UP, cell_to_partition)
-    _ordered_outline_8d_yeet(tiles, outline, start_pos, Vector2i.RIGHT + Vector2i.DOWN, cell_to_partition)
-    
-    return tiles
-
-func _ordered_outline_4d_yeet(tiles: Array, outline: Dictionary, start_pos: Vector2i, dir: Vector2i) -> Vector2i:
-    if start_pos + dir in outline:
-        tiles.append(start_pos)
-        outline.erase(start_pos)
-        # print("Erasing bbb", start_pos)
-        while start_pos + dir in outline:
-            start_pos += dir
-            tiles.append(start_pos)
-            outline.erase(start_pos)
-            # print("Erasing ccc", start_pos)
-    return start_pos
-
-func _ordered_outline_8d_yeet(tiles: Array, outline: Dictionary, start_pos: Vector2i, dir: Vector2i, cell_to_partition: Dictionary) -> void:
-    if start_pos + dir in outline and cell_to_partition[start_pos] == cell_to_partition[start_pos + dir]:
-        var gen_outline := _generate_ordered_outline(outline, start_pos + dir, cell_to_partition)
-        tiles.append_array(gen_outline)
-
-func _generate_zone_outline(seed_pos: Vector2, partitions: Dictionary, cell_to_partition: Dictionary) -> Dictionary:
-    var seed_tile_pos := map.local_to_map(map.to_local(seed_pos))
-
-    var outline := {}
-
-    var to_check := [seed_tile_pos]
-    var checked := {}
-
-    while to_check.size() > 0:
-        var tile_pos: Vector2i = to_check.pop_back()
-        if tile_pos in checked:
-            continue
-        checked[tile_pos] = true
-
-        var tile_data := map.get_cell_tile_data(tile_pos)
-        if tile_data:
-            outline[tile_pos] = {}
-            partitions[cell_to_partition[tile_pos]] = tile_pos
-            continue
-
-        to_check.append(tile_pos + Vector2i(-1, 0))
-        to_check.append(tile_pos + Vector2i(1, 0))
-        to_check.append(tile_pos + Vector2i(0, 1))
-        to_check.append(tile_pos + Vector2i(0, -1))
-
-    return outline
+func _draw_tile(tile_pos: Vector2i, color: Color) -> void:
+    var line := _spawn_line(line_child, 16.0, color)
+    var p := map.to_global(map.map_to_local(tile_pos))
+    line.add_point(p - Vector2.RIGHT * 8)
+    line.add_point(p + Vector2.RIGHT * 8)
 
 func _spawn_line(parent: Node, width: float, color: Color) -> Line2D:
     var line := Line2D.new()
