95f1d8efad09dfedcafd21da26f63500a0257a66
calcodev <calcopoddev@gmail.com>
2025-07-27 18:56:53 +0300

    asriel: refactor

--- asriel/scripts/messin_around.gd
+++ asriel/scripts/messin_around.gd
@@ -2,63 +2,34 @@
 extends Node
 
 @export var map: TileMapLayer
-@export var line_child: Node
-@export var line_child2: Node
+@export var _debug_view: Node
 
 @export var zones: Array[Marker2D]
-
-# @export var do_the_wobble
 
 @export var generate_zones: bool = false:
     set(value):
         generate_zones = false
         _generate_zones()
 
-func _process(delta: float) -> void:
-    pass
+# var partitions: Dictionary[int, set] = {}
+var _partitions: Dictionary[int, Dictionary] = {}
+var _cell_to_partition: Dictionary[Vector2i, int] = {}
 
 func _generate_zones() -> void:
-    for child in line_child.get_children():
-        line_child.remove_child(child)
-    for child in line_child2.get_children():
-        line_child2.remove_child(child)
+    for child in _debug_view.get_children():
+        _debug_view.remove_child(child)
     
-    # proper space partitioning
-    var cells := map.get_used_cells()
-    var checked := {}
-
-    var partitions := {}
-    var partition_idx := -1
-    var cell_to_partition := {}
-
-    while cells.size() > 0:
-        var cell: Vector2i = cells.pop_back()
-        var to_check := [cell]
-        partition_idx += 1
-        partitions[partition_idx] = {}
-
-        while to_check.size() > 0:
-            var tile_pos: Vector2i = to_check.pop_back()
-            if tile_pos in checked or map.get_cell_tile_data(tile_pos) == null:
-                continue
-            checked[tile_pos] = true
-            cells.erase(tile_pos)
-
-            partitions[partition_idx][tile_pos] = true
-            cell_to_partition[tile_pos] = partition_idx
-
-            to_check.append(tile_pos + Vector2i(-1, 0))
-            to_check.append(tile_pos + Vector2i(1, 0))
-            to_check.append(tile_pos + Vector2i(0, 1))
-            to_check.append(tile_pos + Vector2i(0, -1))
+    _partitions = {}
+    _cell_to_partition = {}
+    _partition_map(map, _partitions, _cell_to_partition)
     
     var colors = []
-    colors.resize(partition_idx + 1)
-    for i in partition_idx + 1:
+    colors.resize(_partitions.size() + 1)
+    for i in colors.size():
         colors[i] = Color.from_rgba8(randi() % 255, randi() % 255, randi() % 255, 255)
 
     for marker in zones:
-        var touched_partitions := _get_partitions_touched_by_free_zone(marker.global_position, cell_to_partition)
+        var touched_partitions := _get_adjacent_partitions(marker.global_position, _cell_to_partition)
 
         for touched_partition in touched_partitions:
             var color = colors[touched_partition]
@@ -67,11 +38,21 @@ func _generate_zones() -> void:
 
             var outline := _trace(start_pos, enter_pos)
             for cell in outline:
-                var line = _spawn_line(line_child, 16.0, color)
-                var pos := map.to_global(map.map_to_local(cell))
-                line.add_point(pos - Vector2.RIGHT * 8.0)
-                line.add_point(pos + Vector2.RIGHT * 8.0)
+                _draw_tile(cell, color)
 
+func _get_outline_outer_normal(tile_pos: Vector2i) -> Vector2i:
+    var normal := Vector2i.ZERO
+    if map.get_cell_tile_data(tile_pos + Vector2i.LEFT):
+        normal += Vector2i.LEFT
+    if map.get_cell_tile_data(tile_pos + Vector2i.RIGHT):
+        normal += Vector2i.RIGHT
+    if map.get_cell_tile_data(tile_pos + Vector2i.UP):
+        normal += Vector2i.UP
+    if map.get_cell_tile_data(tile_pos + Vector2i.DOWN):
+        normal += Vector2i.DOWN
+    return normal
+
+#region Neighbourood Tracing Helpers
 
 func _trace(start: Vector2i, enter: Vector2i) -> Array[Vector2i]:
     var path: Array[Vector2i] = [start]
@@ -85,13 +66,12 @@ func _trace(start: Vector2i, enter: Vector2i) -> Array[Vector2i]:
         for off in n:
             previous = current
             current = pivot + off
-            if _is_solid(current):
+            if map.get_cell_tile_data(current):
                 path.append(current)
                 pivot = current
                 current = previous
                 break
     return path
-
 
 # TODO(calco): this needs MASSIVE improvement lol
 func _get_neighbourhood(diff: Vector2i) -> Array[Vector2i]:
@@ -114,26 +94,37 @@ func _get_neighbourhood(diff: Vector2i) -> Array[Vector2i]:
     b.append_array(a.slice(0, idx))
     return b
 
+#endregion
 
-func _get_edge_contour() -> void:
-    pass
+#region Parition Helpers
 
-func _is_solid(pos: Vector2i) -> bool:
-    return map.get_cell_tile_data(pos) != null
+func _partition_map(tile_map: TileMapLayer, partitions: Dictionary[int, Dictionary], cell_to_partition: Dictionary[Vector2i, int]) -> void:
+    var cells := tile_map.get_used_cells()
+    var checked := {}
 
-func _get_outline_outer_normal(tile_pos: Vector2i) -> Vector2i:
-    var normal := Vector2i.ZERO
-    if map.get_cell_tile_data(tile_pos + Vector2i.LEFT):
-        normal += Vector2i.LEFT
-    if map.get_cell_tile_data(tile_pos + Vector2i.RIGHT):
-        normal += Vector2i.RIGHT
-    if map.get_cell_tile_data(tile_pos + Vector2i.UP):
-        normal += Vector2i.UP
-    if map.get_cell_tile_data(tile_pos + Vector2i.DOWN):
-        normal += Vector2i.DOWN
-    return normal
+    var partition_idx := -1
+    while cells.size() > 0:
+        var cell: Vector2i = cells.pop_back()
+        var to_check := [cell]
+        partition_idx += 1
+        partitions[partition_idx] = {}
 
-func _get_partitions_touched_by_free_zone(pos: Vector2, cell_to_partition: Dictionary) -> Dictionary:
+        while to_check.size() > 0:
+            var tile_pos: Vector2i = to_check.pop_back()
+            if tile_pos in checked or tile_map.get_cell_tile_data(tile_pos) == null:
+                continue
+            checked[tile_pos] = true
+            cells.erase(tile_pos)
+
+            partitions[partition_idx][tile_pos] = true
+            cell_to_partition[tile_pos] = partition_idx
+
+            to_check.append(tile_pos + Vector2i(-1, 0))
+            to_check.append(tile_pos + Vector2i(1, 0))
+            to_check.append(tile_pos + Vector2i(0, 1))
+            to_check.append(tile_pos + Vector2i(0, -1))
+
+func _get_adjacent_partitions(pos: Vector2, cell_to_partition: Dictionary) -> Dictionary:
     var tile_pos := map.local_to_map(map.to_local(pos))
 
     var partitions := {}
@@ -159,8 +150,12 @@ func _get_partitions_touched_by_free_zone(pos: Vector2, cell_to_partition: Dicti
 
     return partitions
 
+#endregion
+
+#region Drawing Helpers
+
 func _draw_tile(tile_pos: Vector2i, color: Color) -> void:
-    var line := _spawn_line(line_child, 16.0, color)
+    var line := _spawn_line(_debug_view, 16.0, color)
     var p := map.to_global(map.map_to_local(tile_pos))
     line.add_point(p - Vector2.RIGHT * 8)
     line.add_point(p + Vector2.RIGHT * 8)
@@ -173,3 +168,5 @@ func _spawn_line(parent: Node, width: float, color: Color) -> Line2D:
     line.width = width
     line.modulate = color
     return line
+
+#endregion
